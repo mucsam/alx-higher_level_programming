bf96c0e1a6fad518ca8f06f733577da0e880c0bf
Samuel <muchokisamuel@gmail.com>
2023-08-29 13:55:00 +0000

    More returns

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     if sentence == "":
-        t = (None, )
+        t = (None, len(sentence))
         return t
     t = (len(sentence), sentence[0])
     return t
