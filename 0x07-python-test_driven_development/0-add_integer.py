00ce8504105bdac3564b3601fdb96c590b772c1e
Samuel <muchokisamuel@gmail.com>
2023-11-23 12:32:12 +0000

    Integers addition

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""A function that adds 2 integers"""
+
+
+def add_integer(a, b=98):
+    """ Returns addition of 2 integers"""
+
+    if (not isinstance(a, int)) and (not isinstance(a, float)):
+        raise TypeError("a must be an integer")
+    elif (not isinstance(b, int)) and (not isinstance(b, float)):
+        raise TypeError("b must be an integer")
+    else:
+        a = int(a)
+        b = int(b)
+        return a + b
