a701aa8fee0f59cfc6a24390dca1bde0498514b0
Samuel <muchokisamuel@gmail.com>
2023-11-28 17:27:44 +0000

    Read file

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Module for the read_file function
+"""
+
+
+def read_file(filename=""):
+    """Reads a text file and prints it to stdout
+    """
+
+    with open(filename, 'r', encoding='UTF8') as f:
+        print(f.read())
