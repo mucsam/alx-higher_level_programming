d223a3b8c7f288d6873c7b8bdbf26b719cdd8603
Samuel <muchokisamuel@gmail.com>
2023-11-30 11:50:39 +0000

    If it's not tested it doesn't work

---
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+"""Module to test base.py
+"""
+
+import unittest
+from models.base import Base
+
+
+class TestBase(unittest.TestCase):
+    
+    def setUp(self):
+        self.b1 = Base()
+        self.b2 = Base()
+        self.b3 = Base()
+        self.b4 = Base(12)
+        self.b5 = Base()
+
+    def test_base(self):
+        self.assertEqual(self.b1.id, 1)
+        self.assertEqual(self.b2.id, 2)
+        self.assertEqual(self.b3.id, 3)
+        self.assertEqual(self.b4.id, 12)
+        self.assertEqual(self.b5.id, 4)
+
+if __name__ == "__main__":
+    unittest.main()
