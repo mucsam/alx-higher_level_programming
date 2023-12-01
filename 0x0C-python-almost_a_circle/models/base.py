680cf319e0d971b98d39ad9bed40b93684a97243
Samuel <muchokisamuel@gmail.com>
2023-12-01 20:56:11 +0000

    Dictionary to JSON string

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -25,4 +25,5 @@ class Base:
 
         if list_dictionaries is None:
             return "[]"
-        return json.dumps(list_dictionaries)
+        else:
+            return json.dumps(list_dictionaries)
