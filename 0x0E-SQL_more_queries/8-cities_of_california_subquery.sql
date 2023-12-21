9262e61ed74aa45957d82d50951668aab97a21c3
Samuel <muchokisamuel@gmail.com>
2023-12-21 21:37:40 +0000

    Cities of California

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,4 +1,8 @@
 -- lists all the cities of California that can be found in the database hbtn_0d_usa
-SELECT cities.name, cities.id
-FROM states, cities
-WHERE states.name = 'California';
+SELECT id, name
+FROM cities
+WHERE state_id = (
+    SELECT id 
+    FROM states
+    WHERE name = 'California'
+);
