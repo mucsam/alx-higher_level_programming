99898720fe98c3fa83bd5cf1e63a8d64d555f5b5
Samuel <muchokisamuel@gmail.com>
2023-12-22 09:02:11 +0000

    No genre

---
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -0,0 +1,7 @@
+-- lists all shows contained in hbtn_0d_tvshow
+SELECT ts.title, tsg.genre_id
+FROM tv_shows ts
+LEFT JOIN tv_show_genres tsg
+ON ts.id = tsg.show_id
+WHERE tsg.genre_id IS NULL
+ORDER BY ts.title, tsg.genre_id ASC;
